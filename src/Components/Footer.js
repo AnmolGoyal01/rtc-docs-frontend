dbba87a3e973098c333a600eb3c7554f92f38d0f
Priyanka Kumari <priyankakumari_ee21b9_01@dtu.ac.in>
2025-06-12 13:42:11 +0530

    sign-up

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer=()=>{
+    return(
+        <div className="footer-ul">
+            <h3>Collab-Desk</h3>
+        </div>
+    )
+}
+
+export default Footer;
